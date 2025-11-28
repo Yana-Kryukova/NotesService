052b767354ddad7c2e01cebaf1331db84ed65630
Yana-Kryukova <krjanik@icloud.com>
2025-11-28 17:27:53 +0700

    #12 В Apllication Layer добавлен проект NotesService.Application.Services Добавлен IApplicationService и IUsersService

---
+++ NotesService/Application/NotesService.Application.Services/Abstractions/Base/IApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using NotesService.Application.Models.Base;
+
+namespace NotesService.Application.Services.Abstractions.Base;
+
+public interface IApplicationService<TModel, in TId>
+     where TModel : IModel<TId>
+     where TId : struct, IEquatable<TId>
+{
+    Task<TModel> GetUserByIdAsync(TId id);
+
+    Task<IEnumerable<TModel>> GetUsersAsync();
+
+    Task<bool> CreateUserAsync(ICreateModel userInformation);
+
+    Task<bool> UpdateUserAsync(TModel user);
+
+    Task<bool> DeleteUserAsync(TId id);
+}
+
---
+++ NotesService/Application/NotesService.Application.Services/Abstractions/IUsersApplicationService.cs
@@ -0,0 +1,10 @@
+﻿using NotesService.Application.Models.User;
+using NotesService.Application.Services.Abstractions.Base;
+
+namespace NotesService.Application.Services.Abstractions;
+
+public interface IUsersApplicationService : IApplicationService<UserModel, Guid>
+{
+
+    Task<UserModel?> GetUserByUsernameAsync(string username);
+}
