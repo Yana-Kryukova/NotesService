522942a546c045244ab2bebeeb8cea486c1eb53a
Yana-Kryukova <krjanik@icloud.com>
2025-11-28 17:43:57 +0700

    #14 Done Добавлен AutoMapper и настроен маппинг в ApplicationProfile

---
+++ NotesService/Application/NotesService.Application.Services/Mapping/ApplicationProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NotesService.Application.Models.Base;
+using NotesService.Application.Models.Note;
+using NotesService.Application.Models.User;
+using NotesService.Domain;
+using System.Security.Cryptography;
+
+namespace NotesService.Application.Services.Mapping;
+
+public class ApplicationProfile : Profile
+{
+    public ApplicationProfile()
+    {
+        CreateMap<User, UserModel>()
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username.Value));
+
+        CreateMap<Note, NoteModel>()
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title!.Value))
+            .ForMember(dest => dest.Thesis, opt => opt.MapFrom(src => src.Thesis.Value));
+    }
+}
